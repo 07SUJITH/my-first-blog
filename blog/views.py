fbec917aca2964e4f224e3c3795ec29bbbfff82f
07SUJITH <tssujith2002@gmail.com>
2025-06-03 18:58:56 +0530

    feat: Implement post list view rendering

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    '''
+    Function to render the post list page.
+    Args:
+        request: The HTTP request object.
+    Returns:
+        HttpResponse: Rendered HTML page for the post list.
+    '''
+    return render(request, 'blog/post_list.html', {})
