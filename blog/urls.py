454f2603d238c43de0fd130cc16f6044eea53746
07SUJITH <tssujith2002@gmail.com>
2025-06-04 21:35:17 +0530

    feat: Add URL patterns for about page and post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+
+# Assigns a URL namespace to this app so you can reverse namespaced URLs (e.g. 'blog:post_list') without collisions.
+app_name = 'blog' # Namespace for the blog app
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('about/', views.about, name='about'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
