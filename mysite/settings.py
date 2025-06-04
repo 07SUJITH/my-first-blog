e171f41c14019aee33109f517b0ee4321a4af4db
07SUJITH <tssujith2002@gmail.com>
2025-06-04 21:39:12 +0530

    feat: Add POSTS_PER_PAGE  for pagination control

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,3 +151,5 @@ STATIC_ROOT = BASE_DIR / 'static'  # filesystem path where `collectstatic` gathe
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+POSTS_PER_PAGE = 6
