177a104c08560a2e4f0eb2b07bfbd46597cdeab3
07SUJITH <tssujith2002@gmail.com>
2025-06-06 19:46:40 +0530

    feat: add new routes for creating and editing blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,7 @@ app_name = 'blog' # Namespace for the blog app
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('about/', views.about, name='about'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
