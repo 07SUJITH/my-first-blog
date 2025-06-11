9d6eb494d2579a6f9283f6bd2a45657a2a9572ff
07SUJITH <tssujith2002@gmail.com>
2025-06-12 02:01:11 +0530

    feat(forms): add CommentForm for creating and updating comments on blog posts

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
     '''
@@ -13,4 +13,29 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'text']
         
-        
+class CommentForm(forms.ModelForm):
+    '''
+    Form for creating and updating comments on blog posts.
+    '''
+    class Meta:
+        '''
+        Meta class to define the model and fields for the form.
+        Helps django to automatically generate form fields based on the model.
+        '''
+        model = Comment
+        fields = ['text']
+        widgets = {
+            'text': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+        }
+        labels = {
+            'text': 'Comment',
+        }
+        help_texts = {
+            'text': 'Enter your comment here.',
+        }
+        error_messages = {
+            'text': {
+                'required': 'This field is required.',
+                'max_length': 'Comment is too long. Maximum 500 characters allowed.',
+            },
+        }
